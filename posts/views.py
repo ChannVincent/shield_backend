31c16ccab42c52e95230245e48e6e70c418b24c0
Vincent Chann <>
2024-12-29 14:58:25 +0100

    first generated post

--- posts/views.py
+++ posts/views.py
@@ -2,7 +2,9 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 from .models import Post
+from security_data.models import *
 import json
+from django.core import serializers
 
 # View to get all posts by commune : http://localhost:8000/posts/?commune_id=0
 @require_http_methods(["GET"])
@@ -11,6 +13,7 @@ def get_all_posts(request):
     if not commune_id:
         return JsonResponse({'error': 'commune_id is required'}, status=400)
     try:
+        auto_post(commune_id=commune_id)
         posts = Post.objects.filter(commune_id=commune_id).order_by('-id').values()
         return JsonResponse(list(posts), safe=False) 
     except Exception as e:
@@ -48,4 +51,43 @@ def create_post(request):
 
 # create automatic post from security data
 def auto_post(commune_id):
-    pass
+    saved_posts_count = Post.objects.filter(commune_id=commune_id).count()
+    if saved_posts_count > 0:
+        return
+    
+    # 1st class: Usage de stupéfiants
+    posts = []
+    aggression_class = "Usage de stupéfiants"
+    commune = Commune.objects.get(pk=commune_id)
+    title = aggression_class + " à " + commune.name_full
+    securite_records = Securite.objects.filter(commune_id=commune_id, agression_class=aggression_class)
+    
+    # Convert to simplified JSON format
+    simplified_securite_data = [
+        {
+            "commune": record.commune.pk,
+            "year": record.year,
+            "agression_class": record.agression_class,
+            "aggression_unity": record.aggression_unity,
+            "public_value": record.public_value,
+            "facts_value": record.facts_value,
+            "per_thousand": record.per_thousand,
+            "pop": record.pop,
+            "millpop": record.millpop,
+        }
+        for record in securite_records
+    ]
+    
+    # Convert to JSON
+    securite_json = json.dumps(simplified_securite_data)
+    
+    # Create a new post
+    new_post = Post.objects.create(
+        commune_id=commune_id,
+        title=title,
+        text=title,
+        json_data=securite_json,
+    )
+    
+    posts.append(new_post)
+    return posts
