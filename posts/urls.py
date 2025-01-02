0a3c16c69a3ce784bac7fd121b26d799005b1635
Vincent Chann <>
2025-01-02 15:38:48 +0100

    fix comments url

--- posts/urls.py
+++ posts/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('create/', views.create_post, name='create_post'),  # Endpoint to create a new post
     path('<int:post_id>/like/', views.toggle_like, name='toggle_like'),
     path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
-    path('<int:post_id>/comment/', views.get_comments, name='get_comment'),
+    path('<int:post_id>/comments/', views.get_comments, name='get_comment'),
 ]
